b5075dc146fb667cd734b6ca591423ef768a97a6
sai kumar <saikumar85408@gmail.com>
2023-02-20 10:32:49 +0000

    gettting error

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const dbPath = path.join(__dirname, "moviesData.db");
+
+let db = null;
+
+const initializeDb = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDb();
+
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+//Get all rows
+
+app.get("/movies/", async (request, response) => {
+  const getMovieNamesQuery = `
+ SELECT
+ movie_name
+ FROM
+ movie;`;
+  const moviesArray = await db.all(getMovieNamesQuery);
+  response.send(moviesArray);
+});
+
+app.post("/movies/", async (requst, response) => {
+  const movieDetails = requst.body;
+  const { directorId, movieName, leadActor } = movieDetails;
+  const addMovieQuery = `
+    INSERT INTO movie (director_id,movie_name,lead_actor) 
+    VALUES 
+    (${directorId},${movieName},${leadActor})`;
+
+  const dbResponse = await db.run(addMovieQuery);
+  response.send("Movie Successfully Added");
+});
+module.exports = app;
